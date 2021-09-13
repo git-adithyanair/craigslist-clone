28da8e26a4ecdd3822401783a447b7f8fc1775f2
git-adithyanair <adithya.0800@gmail.com>
2021-09-14 02:57:56 +0400

    Changes for deploy to Heroku.

--- craigslist_clone/settings.py
+++ craigslist_clone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +128,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
  
+django_heroku.settings(locals())
